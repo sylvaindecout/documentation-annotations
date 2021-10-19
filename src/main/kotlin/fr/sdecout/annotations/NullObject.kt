ce1b36fd52712b00e08d23c4209378ee6154ef5d
Sylvain Decout <sylvain.decout+github@gmail.com>
2021-10-19 14:55:02 +0200

    :sparkles: Add annotation for Null Object pattern

---
+++ src/main/kotlin/fr/sdecout/annotations/NullObject.kt
@@ -0,0 +1,12 @@
+package fr.sdecout.annotations
+
+import java.lang.annotation.Inherited
+
+/**
+ * Represents a Null Object, which is to be used instead of null.
+ *
+ * @see <a href="https://refactoring.guru/introduce-null-object">Refactoring.Guru</a>
+ */
+@Inherited
+@MustBeDocumented
+annotation class NullObject
