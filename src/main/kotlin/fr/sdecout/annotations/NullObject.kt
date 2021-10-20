e11c70a1681c5e4287bc4e1d6d5b9b7dc67eab4d
Sylvain Decout <sylvain.decout+github@gmail.com>
2021-10-20 18:52:09 +0200

    Add annotation for Exceptional Value pattern

--- src/main/kotlin/fr/sdecout/annotations/NullObject.kt
+++ src/main/kotlin/fr/sdecout/annotations/NullObject.kt
@@ -6,6 +6,7 @@ import java.lang.annotation.Inherited
  * Represents a Null Object, which is to be used instead of null.
  *
  * @see <a href="https://refactoring.guru/introduce-null-object">Refactoring.Guru</a>
+ * @see fr.sdecout.annotations.ExceptionalValue
  */
 @Inherited
 @MustBeDocumented
