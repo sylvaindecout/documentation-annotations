8ebbc1d9b151f436bc6437825ec315a5105596a7
Sylvain Decout <sylvain.decout+github@gmail.com>
2021-10-22 18:29:26 +0200

    :sparkles: Add annotation for Adapter pattern

---
+++ src/main/kotlin/fr/sdecout/annotations/Adapter.kt
@@ -0,0 +1,15 @@
+package fr.sdecout.annotations
+
+import kotlin.annotation.AnnotationTarget.CLASS
+import kotlin.reflect.KClass
+
+/**
+ * Marks this class as an implementation of Adapter design pattern.
+ *
+ * Strategy is a structural pattern defined by GoF in *Design Patterns: Elements of Reusable Object-Oriented Software*.
+ *
+ * @see <a href="https://refactoring.guru/design-patterns/adapter">Refactoring.Guru</a>
+ */
+@MustBeDocumented
+@Target(CLASS)
+annotation class Adapter(val targetType: KClass<*>, val adaptee: KClass<*>)
